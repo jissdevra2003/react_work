1a955cebe922ed182149ff43a145f611cd3ccd1f
Jiss devra <96423636+jissdevra2003@users.noreply.github.com>
2025-01-15 22:24:26 +0530

    index gitdata removed

--- express_learning/index.js
+++ express_learning/index.js
@@ -3,41 +3,7 @@ const express=require('express')
 const app=express()
 const port=3000
 
-const gitData={
-  "login": "jissdevra2003",
-  "id": 96423636,
-  "node_id": "U_kgDOBb9O1A",
-  "avatar_url": "https://avatars.githubusercontent.com/u/96423636?v=4",
-  "gravatar_id": "",
-  "url": "https://api.github.com/users/jissdevra2003",
-  "html_url": "https://github.com/jissdevra2003",
-  "followers_url": "https://api.github.com/users/jissdevra2003/followers",
-  "following_url": "https://api.github.com/users/jissdevra2003/following{/other_user}",
-  "gists_url": "https://api.github.com/users/jissdevra2003/gists{/gist_id}",
-  "starred_url": "https://api.github.com/users/jissdevra2003/starred{/owner}{/repo}",
-  "subscriptions_url": "https://api.github.com/users/jissdevra2003/subscriptions",
-  "organizations_url": "https://api.github.com/users/jissdevra2003/orgs",
-  "repos_url": "https://api.github.com/users/jissdevra2003/repos",
-  "events_url": "https://api.github.com/users/jissdevra2003/events{/privacy}",
-  "received_events_url": "https://api.github.com/users/jissdevra2003/received_events",
-  "type": "User",
-  "user_view_type": "public",
-  "site_admin": false,
-  "name": "Jiss devra ",
-  "company": null,
-  "blog": "",
-  "location": "UJJAIN",
-  "email": null,
-  "hireable": null,
-  "bio": null,
-  "twitter_username": null,
-  "public_repos": 3,
-  "public_gists": 0,
-  "followers": 0,
-  "following": 0,
-  "created_at": "2021-12-20T12:12:11Z",
-  "updated_at": "2025-01-08T12:44:47Z"
-}
+
 
 app.get('/',(req,res)=>{
 res.send("Hello jiss,you are good")
@@ -54,9 +20,9 @@ res.send(`<h3>Messi is from argentina</h3>`)
 app.get('/neymar',(req,res)=>{
 res.send(`<h3>Neymar is from Brazil</h3>`)
 })
-app.get('/github',(req,res)=>{
-res.json(gitData)
-})
+// app.get('/github',(req,res)=>{
+// res.json(gitData)
+// })
 
 app.listen(process.env.PORT,()=>{
 console.log(`Watching friends ${process.env.PORT}`)
